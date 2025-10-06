ce46317bf9ff7a25930710dabb85974a3ee5bd53
kian-moslemi <kian.moslemi@gmail.com>
2025-10-06 21:43:28 +0330

    first commit

--- club/settings.py
+++ club/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_0mum$^#6%16l3oeq8kc+y24daufb$0oa6wr$xht8$*&aa@c4t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
